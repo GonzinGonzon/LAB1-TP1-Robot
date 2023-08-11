9413d914ebe4e3ea512abd9d16ade4096f30d9e5
GMTUDS <142052001+GMTUDS@users.noreply.github.com>
2023-08-11 19:17:50 -0300

    Add class Bateria

---
+++ src/lab1/tp1/robot/Batería.java
@@ -0,0 +1,21 @@
+
+package lab1.tp1.robot;
+
+
+public class Batería {
+  
+    private int carga;
+     public Batería (int carga) {
+         
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+     
+    
+}
