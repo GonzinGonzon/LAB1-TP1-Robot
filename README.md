e6554fbad043a09eafe1fcf9a2b11d4bba5960c3
Gonzalo Amieva García <gonza.amieva@gmail.com>
2023-08-11 18:55:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LAB1-TP1-Robot
+ Laboratorio 1 -Trabajo 1
---
+++ src/lab1/tp1/robot/LAB1TP1Robot.java
@@ -0,0 +1,17 @@
+
+
+
+package lab1.tp1.robot;
+
+/**
+ *
+ * @author gonza
+ */
+public class LAB1TP1Robot {
+
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
